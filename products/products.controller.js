ddf580d8eec6566b6a16ac5187a5a4bb09d56356
ndjerrou <n.djerrou@gmail.com>
2022-12-10 10:49:17 +0100

    MVC Pattern implemented

--- products/products.controller.js
+++ products/products.controller.js
@@ -2,7 +2,7 @@ const Product = require('./products.model');
 const _ = require('underscore');
 
 module.exports = {
-  async fetchProducts(req, res) {
+  async fetchAllProducts(req, res) {
     try {
       const products = await Product.find();
       res.send({
@@ -17,12 +17,14 @@ module.exports = {
     }
   },
   async fetchSingleProduct(req, res) {
-    // products/:id
-    // findById ==> mongoose
     try {
       const product = await Product.findById(req.params.id).exec();
 
-      // @TODO : check if product is not found, if id is incorrect
+      if (!product)
+        return res.status(400).send({
+          ok: false,
+          msg: 'Product not found with the given id',
+        });
 
       res.send({ ok: true, data: _.pick(product, ['name', 'price', 'desc']) });
     } catch (err) {
@@ -51,6 +53,54 @@ module.exports = {
       });
     } catch (err) {
       res.status(500).send({
+        ok: false,
+        msg: err,
+      });
+    }
+  },
+  async updateProduct(req, res) {
+    const { id } = req.params;
+    console.log('ici');
+    try {
+      const updatedProduct = await Product.findByIdAndUpdate(id, req.body, {
+        new: true,
+      }).exec();
+
+      if (!updatedProduct)
+        return res.status(400).send({
+          ok: false,
+          msg: 'Product not found with the given id',
+        });
+
+      res.send({
+        ok: true,
+        data: updatedProduct,
+      });
+    } catch (err) {
+      return res.status(500).status({
+        ok: false,
+        msg: err,
+      });
+    }
+  },
+  async deleteProduct(req, res) {
+    const id = req.params.id;
+
+    try {
+      const deletedProduct = await Product.findByIdAndDelete(id);
+
+      if (!deletedProduct)
+        return res.status(400).send({
+          ok: false,
+          msg: 'Product not found with the given id',
+        });
+
+      res.send({
+        ok: true,
+        data: deletedProduct,
+      });
+    } catch (err) {
+      return res.status(500).status({
         ok: false,
         msg: err,
       });
--- products/products.router.js
+++ products/products.router.js
@@ -2,54 +2,21 @@ const express = require('express');
 
 const validatePayload = require('../middlewares/validatePayload');
 const {
-  fetchProducts,
+  fetchAllProducts,
   addProduct,
   fetchSingleProduct,
+  updateProduct,
+  deleteProduct,
 } = require('./products.controller');
 
 const router = express.Router();
 
-router.route('/').get(fetchProducts).post(validatePayload, addProduct);
+router.route('/').get(fetchAllProducts).post(validatePayload, addProduct);
 
 router
   .route('/:id')
   .get(fetchSingleProduct)
-  .put(validatePayload, (req, res) => {
-    const { id } = req.params;
-
-    const product = products.find((product) => product.id === +id);
-
-    if (!product)
-      return res.status(400).send({
-        ok: false,
-        message: 'Product not found with the given id',
-      });
-
-    for (let key in req.body) {
-      product[key] = req.body[key];
-    }
-
-    res.send({ ok: true, data: product });
-  })
-  .delete((req, res) => {
-    const id = +req.params.id;
-
-    const idx = products.findIndex((product) => product.id === id);
-
-    console.log(idx);
-
-    if (idx < 0)
-      return res.status(404).send({
-        ok: false,
-        message: 'Product not found with the given ID',
-      });
-
-    const [deletedProduct] = products.splice(idx, 1);
-
-    res.send({
-      ok: true,
-      data: deletedProduct,
-    });
-  });
+  .put(validatePayload, updateProduct)
+  .delete(deleteProduct);
 
 module.exports = router;
